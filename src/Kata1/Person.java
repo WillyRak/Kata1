f2cddc58cb8f0249b0916cd094bd496e781b7e25
Willy <thewillyeb@gmail.com>
2023-12-20 15:05:33 +0000

    Finished Person class and Kata 1

---
+++ src/Kata1/Person.java
@@ -0,0 +1,30 @@
+package Kata1;
+
+import java.time.LocalDate;
+
+public class Person {
+
+    private final String name;
+    private final LocalDate birthday;
+
+    public Person(String name, LocalDate birthday){
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public int getAge(){
+        return toYears(LocalDate.now().toEpochDay() - this.birthday.toEpochDay());
+    }
+
+    private int toYears(long days) {
+        return (int) (days/365.25);
+    }
+}
